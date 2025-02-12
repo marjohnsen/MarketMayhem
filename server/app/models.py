43b55f97970b18f7e561e06f37230975e700b430
Marius Johnsen <marius.johnsen@outlook.com>
2025-02-12 21:34:03 +0100

    simplify sqlalchemy models

--- server/app/models.py
+++ server/app/models.py
@@ -4,48 +4,39 @@ from app.extensions import db
 
 
 class GameState(enum.Enum):
-    LOBBY = "lobby"
-    IN_PROGRESS = "active"
-    FINISHED = "finished"
+    PENDING = "pending"
+    ONGOING = "ongoing"
+    GAME_OVER = "game_over"
 
 
 class PlayerState(enum.Enum):
-    ACTIVE = "active"
+    IN_LOBBY = "in_lobby"
+    IN_GAME = "in_game"
     BANNED = "banned"
 
 
-class Game(db.Model):
-    """
-    The Game model represents a game session. It has a key to identify the game and a state to track the game's state.
-    """
+class ConnectionState(enum.Enum):
+    CONNECTED = "connected"
+    DISCONNECTED = "disconnected"
 
-    __tablename__ = "game"
+
+class Game(db.Model):
+    __tablename__ = "games"
 
     id = db.Column(db.Integer, primary_key=True)
     key = db.Column(db.String(8), unique=True, nullable=False, default=lambda: uuid.uuid4().hex[:8])
-    state = db.Column(db.Enum(GameState), nullable=False, default=GameState.LOBBY)
+    state = db.Column(db.Enum(GameState), nullable=False, default=GameState.PENDING)
 
     players = db.relationship("Player", backref="game", lazy=True, cascade="all, delete-orphan")
 
-    def __repr__(self):
-        player_count = db.session.query(Player).filter_by(game_id=self.id).count()
-        return f"<Game (Key: {self.key}, State: {self.state}, Players: {player_count})>"
-
 
 class Player(db.Model):
-    """
-    The Player model represents a player in a game. It has a name and a reference to the game it is in.
-    """
-
-    __tablename__ = "player"
+    __tablename__ = "players"
 
     id = db.Column(db.Integer, primary_key=True)
-    key = db.Column(db.String(8), unique=True, nullable=False, default=lambda: uuid.uuid4().hex[:8])
     name = db.Column(db.String(20), nullable=False)
-    state = db.Column(db.Enum(PlayerState), nullable=False, default=PlayerState.ACTIVE)
-    game_id = db.Column(db.Integer, db.ForeignKey("game.id"), nullable=False, index=True)
+    state = db.Column(db.Enum(PlayerState), nullable=False, default=PlayerState.IN_LOBBY)
+    connection = db.Column(db.Enum(ConnectionState), nullable=False, default=ConnectionState.CONNECTED)
+    gameplay_id = db.Column(db.Integer, db.ForeignKey("games.id"), nullable=True, index=True)
 
-    __table_args__ = (db.UniqueConstraint("name", "game_id", name="uq_player_name_game"),)
-
-    def __repr__(self):
-        return f"<Player {self.name} (Key: {self.key}, State: {self.state})>"
+    __table_args__ = (db.UniqueConstraint("name", "gameplay_id", name="uq_player_name_gameplay"),)
