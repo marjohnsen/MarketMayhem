21dc4b5f496db12c02a5fc663dfd241251f3e12d
Marius Johnsen <mariuj@equinor.com>
2025-02-14 13:41:00 +0100

    remove price_series function to reduce complexity

--- server/engine/market_simulator.py
+++ server/engine/market_simulator.py
@@ -1,9 +1,7 @@
 import numpy as np
 
 
-def get_next_price(
-    last_price: float, volatility: float, buy_volume: int, sell_volume: int
-) -> float:
+def get_next_price(last_price: float, volatility: float, buy_volume: int, sell_volume: int) -> float:
     """
     Simulate the next log return and update the price based on user input data
     and random noise.
@@ -37,31 +35,3 @@ def get_next_price(
     next_price = last_price * np.exp(log_return)
 
     return next_price
-
-
-def get_price_series(last_price, volatility, buy_volume, sell_volume, N):
-    if N == 0:
-        return []
-    next_price = get_next_price(last_price, volatility, buy_volume, sell_volume)
-    return [last_price] + get_price_series(
-        next_price, volatility, buy_volume, sell_volume, N - 1
-    )
-
-
-if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-
-    # parameters
-    last_price = 100
-    volatility = 0.01
-    buy_volume = 1
-    sell_volume = 100
-    N = 10
-
-    price_series = get_price_series(last_price, volatility, buy_volume, sell_volume, N)
-
-    plt.plot(price_series)
-    plt.title("Generated Price Sequence (100 Steps)")
-    plt.xlabel("Step")
-    plt.ylabel("Price")
-    plt.show()
