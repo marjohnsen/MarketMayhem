bc2ef53fa51df77a8a380d9deaebd391be06e0f7
Marius Johnsen <mariuj@equinor.com>
2025-06-24 14:24:56 +0200

    draw header to stdscr and draw the canvas underneat. Add support for auto resize.

--- client/main.py
+++ client/main.py
@@ -1,30 +1,62 @@
 import curses
+from ui.navigate import Navigation
 from ui.canvas import Canvas
-from ui.navigate import Navigate
-from ui.palette import init_pairs, Pairs
 from ui.header import Header
+from ui.palette import init_pairs, Pairs
 
 
 def main(stdscr):
+    # Initalize
     curses.curs_set(0)
-    stdscr.nodelay(True)
-    stdscr.timeout(100)
     init_pairs()
 
-    canvas = Canvas(stdscr, h=10, w=40, y=1, x=1)
-    header = Header("ui/ascii_art/marketmayhem.txt")
+    header = Header.from_file("ui/ascii_art/marketmayhem.txt")
+    canvas = Canvas(stdscr, len(header.lines) + 1)
+
+    choices = ["Join Game", "Host Menu", "Exit"]
+    navigate = Navigation(len(choices))
+
+    stdscr.nodelay(True)
+    stdscr.timeout(100)
+
+    canvas.nodelay(True)
+    canvas.timeout(100)
 
     while True:
-        stdscr.erase()
+        # Navigation
+        key = canvas.getch()
+        action = navigate(key)
 
-        stdscr.border()
-        header.draw(stdscr, y=0, pair=Pairs.STANDARD)
+        # Rebuild on resize
+        if key == curses.KEY_RESIZE:
+            canvas.rebuild()
+            continue
+
+        # Clear previous frame
+        stdscr.erase()
+        canvas.erase()
+
+        # Stage the header
+        header.draw(stdscr, 1, Pairs.STANDARD)
         stdscr.noutrefresh()
 
-        canvas.clear()
-        canvas.win.border()
+        # Stage the canvas
+        canvas.draw_list(choices, selected=navigate.pos, pair=Pairs.STANDARD)
+        canvas.noutrefresh()
 
-        canvas.flush()
+        # Make decisions based on navigation
+        if action == Navigation.SELECT:
+            match choices[navigate.pos]:
+                case "Exit":
+                    break
+                case "Join Game":
+                    break
+                case "Host Menu":
+                    break
+        elif action == Navigation.BACK:
+            break
+
+        # Push frame to the terminal
         curses.doupdate()
 
 
